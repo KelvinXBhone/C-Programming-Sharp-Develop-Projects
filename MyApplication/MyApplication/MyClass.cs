0a7cfa644a6e1f5135b5a07a94c6f553a7a1169b
K3LV!N <124446891+KelvinXBhone@users.noreply.github.com>
2025-08-25 19:51:24 +0700

    Add files via upload

---
+++ MyApplication/MyApplication/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Kelvin
+ * Date: 11/30/2023
+ * Time: 2:19 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace MyApplication
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
