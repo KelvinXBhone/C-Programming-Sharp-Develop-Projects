94c918ea1c914b4a3e60137fa81eed64d5414ce9
K3LV!N <124446891+KelvinXBhone@users.noreply.github.com>
2025-08-25 19:54:24 +0700

    Add files via upload

---
+++ fitnesstracker/StartForm.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Kelvin
+ * Date: 12/21/2023
+ * Time: 3:26 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace fitnesstracker
+{
+	/// <summary>
+	/// Description of StartForm.
+	/// </summary>
+	public partial class StartForm : Form
+	{
+		public StartForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
